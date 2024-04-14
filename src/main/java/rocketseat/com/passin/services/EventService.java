40b347d2d6245bf0cf07ed05cde43aa663131070
Eliel Silva <elielbarros.silva@gmail.com>
2024-04-14 16:08:36 -0300

    Criação do EventService e seu primeiro metodo, getEventDetail

---
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -0,0 +1,20 @@
+package rocketseat.com.passin.services;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.repositories.EventRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    public void getEventDetail(String eventId) {
+        Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with ID: " + eventId));
+        return;
+    }
+}
